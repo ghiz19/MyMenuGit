22a61477b4b536c1ca62160cf4316f3d634f2796
ghiz19 <ghizlane123.el@gmail.com>
2021-03-30 01:50:46 +0200

    my First Commit

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,17 +1,26 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
-
+    	System.out.println("Choix menu");
+    	System.out.println("1 - poulet");
+    	System.out.println("2 - boeuf");
+    	System.out.println("3 - végétarienl");
+    	System.out.println("Que souhaitez-vous comme menu ?");
     }
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-
+    	
+    	System.out.println("Vous avez choisi le menu "+ nbMenu);
+    	
+    	
     }
 }
--- test/com/ocr/anthony/OrderTest.java
+++ test/com/ocr/anthony/OrderTest.java
@@ -1,13 +1,13 @@
 package com.ocr.anthony;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 public class OrderTest {
