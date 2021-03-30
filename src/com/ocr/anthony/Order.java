10bc6063b92a5785ee3b8667a433bec8cb145d87
ghiz19 <ghizlane123.el@gmail.com>
2021-03-30 17:20:20 +0200

    switch

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -19,8 +19,23 @@ public class Order {
      */
     public void displaySelectedMenu(int nbMenu) {
     	
-    	System.out.println("Vous avez choisi le menu "+ nbMenu);
+    	switch(nbMenu) {
+    	case 1 :
+    		System.out.println("Vous avez choisi le menu : poulet"); 
+    		break;
+    	case 2 :
+    		System.out.println("Vous avez choisi le menu : boeuf");
+    		break;
+    	case 3 :
+    		System.out.println("Vous avez choisi le menu : végétarienl");
+    		break;
+    	 default:
+             System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+             break;
+    	}
     	
+    	
+    
     	
     }
 }
